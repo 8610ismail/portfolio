94a1fab10c977038164685ca71eb59bc410d4222
John Doe <john.doe@example.com>
2024-10-17 13:56:27 +0530

    hii

--- script.js
+++ script.js
@@ -13,11 +13,14 @@ document.addEventListener("DOMContentLoaded", () => {
 const hamburger = document.getElementById('hamburger');
 const hamnav = document.getElementById('ham-nav');
 const nav = document.getElementById('nav');
+const header = document.getElementById('header');
 hamburger.addEventListener('click',()=>{
     if (hamnav.style.display === 'flex') {
         hamnav.style.display = 'none';
+        header.style.height = 'auto';
     } else {
         hamnav.style.display = 'flex';
+        header.style.height = '400px';
     }
 });
 
